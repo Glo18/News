702b91d6b428e6fae419d15cd4b7c120ff18d4dc
Glo18 <kiogoraspecial@gmail.com>
2020-12-15 14:14:55 +0300

    refactor

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.util.List;
+
 public class Department {
+    private int id;
+    private String departmentName;
+    private String departmentDescription;
+    private int numberOfEmployees;
+    private List<Users> departmentUsers;
+    private List<DepartmentNews> departmentNews;
 }

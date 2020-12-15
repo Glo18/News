54fd8a9c1f0784843f801fc03d256ad46fc6688f
Glo18 <kiogoraspecial@gmail.com>
2020-12-16 00:52:48 +0300

    create test in departmentnews

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class DepartmentNews {
     private int id;
     private int departmentId;
@@ -13,4 +15,63 @@ public class DepartmentNews {
         this.news = news;
         this.author = author;
     }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNews() {
+        return news;
+    }
+
+    public void setNews(String news) {
+        this.news = news;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DepartmentNews)) return false;
+        DepartmentNews that = (DepartmentNews) o;
+        return getId() == that.getId() &&
+                Objects.equals(getDepartmentId(), that.getDepartmentId()) &&
+                Objects.equals(getTitle(), that.getTitle()) &&
+                Objects.equals(getNews(), that.getNews()) &&
+                Objects.equals(getAuthor(), that.getAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getDepartmentId(), getTitle(), getNews(), getAuthor());
+    }
 }
+
--- src/test/java/models/DepartmentNewsTest.java
+++ src/test/java/models/DepartmentNewsTest.java
@@ -1,7 +1,36 @@
 package models;
 
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class DepartmentNewsTest extends TestCase {
+import static org.junit.Assert.*;
 
+public class DepartmentNewsTest  {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    public void instantiatesDepartmentNewsDetails() throws Exception{
+        DepartmentNews testDepartmentNews = setUpDepartmentNews();
+        assertEquals(12, testDepartmentNews.getDepartmentId());
+        assertEquals("Leave Days", testDepartmentNews.getTitle());
+        assertEquals("Leave Management", testDepartmentNews.getNews());
+        assertEquals("Glory", testDepartmentNews.getAuthor());
+    }
+
+    @Test
+    public void setId(){
+        DepartmentNews testDepartmentNews = setUpDepartmentNews();
+        testDepartmentNews.setId(4);
+        assertEquals(4,testDepartmentNews.getId());
+    }
+
+    public DepartmentNews setUpDepartmentNews(){
+        return new DepartmentNews (12, "Leave Days", "Leave Management", "Glory");
+    }
 }
